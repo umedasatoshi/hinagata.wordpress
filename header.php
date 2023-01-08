2b16cd3c6dd73a6027d5df4e3252f4c6cd19f425
umedasatoshi <umeda0@gmail.com>
2023-01-08 19:10:28 +0900

    Update header.php

--- header.php
+++ header.php
@@ -8,18 +8,19 @@
 <meta property="og:title" content="<?php bloginfo('name'); ?><?php if(!is_home()) { echo ' | '; wp_title();} ?>">
 <meta property="og:description" content="<?php bloginfo('description'); ?>">
 <meta property="og:url" content="<?php echo esc_url(home_url()) ; ?>">
-<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/common/img/ogp_default.jpg">
-<meta property="image_src" content="<?php echo get_template_directory_uri(); ?>/common/img/ogp_default.jpg">
+<meta property="og:image" content="<?php get_template_directory_uri(); ?>/common/img/ogp_default.jpg">
+<meta property="image_src" content="<?php get_template_directory_uri(); ?>/common/img/ogp_default.jpg">
 <meta property="og:type" content="website">
 <meta property="og:site_name" content="<?php bloginfo('name'); ?><?php if(!is_home()) { echo ' | '; wp_title();} ?>">
 <meta property="og:site_url" content="<?php echo esc_url(home_url()) ; ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" media="screen,tv" />
-<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/common/img/favicon/favicon.ico" />
+<link rel="shortcut icon" href="<?php get_template_directory_uri(); ?>/common/img/favicon/favicon.ico" />
 <?php if ( is_singular() ) wp_enqueue_script('comment-reply'); ?>
 <?php wp_head(); ?>
 </head>
 <body id="body" <?php body_class(); ?>>
+<?php wp_body_open(); ?>
 <header id="header">
 	<h1><a href="<?php echo esc_url(home_url()) ; ?>"><?php bloginfo('name'); ?></a></h1>
 	<p><?php bloginfo('description'); ?></p>
